a8c4d8c9b15f62f13b40136e817517af94a65ce9
tokarski-to <tomasz.tokarski93@gmail.com>
2024-06-11 14:17:52 +0200

    Day 21: Add description on the new columns

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
-st.set_page_config(layout='wide')
+st.set_page_config(page_title='My portfolio')
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image('images/photo.png')
+    st.image('images/makapaka.png')
 
 with col2:
     st.title('Tomasz Tokarski')
@@ -14,3 +14,7 @@ with col2:
     Beginner programmer building his portfolio. Beginner programmer building his portfolio. Beginner programmer building his portfolio.
     """
     st.info(content)
+intro = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(intro)
